34d3acb9d67d0eacd7a465616fa666f4e0144f4a
南希 <447108518@qq.com>
2019-11-07 09:25:55 +0800

    add swiper

--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 import homeHeader from './component/header'
+import homeSwiper from './component/swiper'
 
 export default {
   name: 'home',
   components: {
-    homeHeader
+    homeHeader,
+    homeSwiper
   }
 }
 </script>
